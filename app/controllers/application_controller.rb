afb6ea87a51f59655a078975ec14180dd8d27757
Zoee Silcock <mrzoee@gmail.com>
2016-10-04 16:10:03 +0200

    Setup some basic forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+  helper_method :user_signed_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  rescue StandardError
+    nil
+  end
+
+  def user_signed_in?
+    return true if current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :users, only: [:new, :create, :update]
+
   get 'hello_world', to: 'hello_world#index'
   get 'home/index'
 

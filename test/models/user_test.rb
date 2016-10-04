a778d157c9528d4694d5d35df2b9eb7e5c27d2b4
Zoee Silcock <mrzoee@gmail.com>
2016-10-04 15:00:09 +0200

    Add a user model

---
+++ test/models/user_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def user_attributes
+    { email: 'test@best.bar', password: 'password123' }
+  end
+
+  test 'it requires an email' do
+    user = User.new user_attributes.merge(email: nil)
+    assert_not user.save, "Saved without an e-mail"
+  end
+
+  test 'the email has to be unique' do
+    user = User.new user_attributes.merge(email: 'user@example.com')
+    assert_not user.save, "Saved with en existing e-mail"
+  end
+
+  test 'it requires a password' do
+    user = User.new user_attributes.merge(password: nil)
+    assert_not user.save, "Saved without a password"
+  end
+
+  test 'it accepts a gamertag' do
+    gamertag = 'carrot_king'
+    user = User.new user_attributes.merge(gamertag: gamertag)
+    assert user.save, "Saved with a gamertag"
+    assert_equal gamertag, user.gamertag
+  end
+end

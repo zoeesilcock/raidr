ffb0aa88b828bc060fa199d47735abdd96ebc7c4
Zoee Silcock <mrzoee@gmail.com>
2016-10-10 11:19:04 +0200

    Change the signup flow

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
     @user = User.new
+
+    if user_signed_in? && current_user.current_group
+      redirect_to current_user.current_group
+    end
   end
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,17 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test "that it gets index" do
     get home_index_url
     assert_response :success
+  end
+
+  test "that it redirects to the current group" do
+    login_user
+
+    user = User.first
+
+    get home_index_url
+    assert_redirected_to user.current_group
   end
 end
